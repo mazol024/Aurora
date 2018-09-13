eb848fafb7cb05ca45ce89289d88be399b0ef5c1
olegmazhanov <oleg.mazhanov@gmail.com>
2018-09-14 07:37:14 +1200

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="app">
+      <br/>
+      <em>Voltage Complaints Form</em>
+      <br/>
+      <br/>
+    <app-complaint></app-complaint>
+    </div>
+    <br/>
+    <div class='app'>
+      <br/>
+      <em> Voltage Complaints Register</em>
+      <br/>
+      <br/>
+      <app-customer-list></app-customer-list>
+    </div>`,
+  styles: ['.app {color: white; background-color:#f58220;margin:3px;padding: 3px;}']
 })
 export class AppComponent {
-  title = 'CustomersList';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list.component';
+import { CustomerComponent } from './customer.component';
+import { AppComplaintComponent } from './app-complaint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    CustomerComponent,
+    AppComplaintComponent
   ],
   imports: [
     BrowserModule
