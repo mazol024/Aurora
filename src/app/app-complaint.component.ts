0584a415f7ca43f3ece9ed1f70da8945fc640ff1
olegmazhanov <oleg.mazhanov@gmail.com>
2018-09-17 10:25:43 +1200

    After refactoring.

--- src/app/app-complaint.component.ts
+++ src/app/app-complaint.component.ts
@@ -10,11 +10,11 @@ import {st} from '@angular/core/src/render3';
   styleUrls: ['app-complaint.component.css']
 })
 export class AppComplaintComponent implements OnInit {
-  customer = {num: '', name: '', address: ''
+  abonent = {num: '', name: '', address: ''
     , date: '', compl: '',
     disstaff: '', advice: '', status: '', category: '', complited: '', distype: ''
   };
-  customerList = [
+  complaintsList = [
     { num: 'CQ3/12', name: 'Brian', address: '8 View St, Dunedin, 9016', date: '22/02/2015', compl: 'Flicking during mornig',
       disstaff: 'NF', advice: '12/03/2015', status: 'Finish', category: 'LV', complited: '12/03/2015', distype: 'Low'},
     { num: '03452', name: 'Mr. Peter', address: '49 Riselaw Rd, Calton Hill, Dunedin 9012', date: '11/12/2011', compl: 'Low voltage',
@@ -30,8 +30,8 @@ export class AppComplaintComponent implements OnInit {
       disstaff: 'NF', advice: '-', status: 'Finish', category: 'JP', complited: '12/05/2015', distype: 'Low'}
   ];
   addComplaint() {
-    this.customerList.push(this.customer);
-    this.customer = {num: '', name: '', address: ''
+    this.complaintsList.push(this.abonent);
+    this.abonent = {num: '', name: '', address: ''
       , date: '', compl: '',
       disstaff: '', advice: '', status: '', category: '', complited: '', distype: ''
     };
@@ -41,17 +41,17 @@ export class AppComplaintComponent implements OnInit {
 
   ngOnInit() {}
   onSelectRow(num, name, address, date, compl, disstaff, advice, status, category, complited, distype ) {
-    /*this.customerList.push({num, name, address, date, compl, disstaff, advice, status, category, complited, distype});*/
-    this.customer.num = num;
-    this.customer.name = name;
-    this.customer.address = address;
-    this.customer.date = date;
-    this.customer.compl = compl;
-    this.customer.disstaff = disstaff;
-    this.customer.advice = advice;
-    this.customer.status = status;
-    this.customer.category = category;
-    this.customer.complited = complited;
-    this.customer.distype = distype;
+    /*this.complaintsList.push({num, name, address, date, compl, disstaff, advice, status, category, complited, distype});*/
+    this.abonent.num = num;
+    this.abonent.name = name;
+    this.abonent.address = address;
+    this.abonent.date = date;
+    this.abonent.compl = compl;
+    this.abonent.disstaff = disstaff;
+    this.abonent.advice = advice;
+    this.abonent.status = status;
+    this.abonent.category = category;
+    this.abonent.complited = complited;
+    this.abonent.distype = distype;
   }
 }
